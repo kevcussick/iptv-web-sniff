1c3d371db4b5824fbb75e88c266ee57575b812df
panda-mute <wxuzju@gmail.com>
2020-05-04 20:07:57 +0800

    add migu support

--- sniff/plugins/migu_live.py
+++ sniff/plugins/migu_live.py
@@ -1,15 +1,13 @@
 from sniff.web_live import web_live
 
-import subprocess
+from urllib.parse import urlparse, urlunparse
 import requests
-import hashlib
 import json
-import time
 import re
 import os
 
 
-class gdtv_live(web_live):
+class migu_live(web_live):
 
     def __init__(self, chname, request_info, extinfo, referer, logger):
 
@@ -18,22 +16,9 @@ class gdtv_live(web_live):
     def sniff_stream(self):
 
         print("probe website %s ......"%(self.website))
-        liveurl = "%s?id=%s"%(self.liveapi, self.chname)
-        epoch = int(time.time())
-        str = "55f703e33e613e9482e1181ca8c71751&Njc4YWVlMTA5YTg1NmRiYzQxZDBlNjhiNGRkMDQ0NWM=&1.0.0&%s"%(epoch)
-        hl = hashlib.md5(str.encode(encoding="utf-8"))
-        headers = {
-
-            "X-API-TIMESTAMP": "%s"%(epoch),
-	    "X-API-KEY": "55f703e33e613e9482e1181ca8c71751",
-	    "X-AUTH-TYPE": "md5",
-	    "X-API-VERSION": "1.0.0",
-	    "X-API-SIGNATURE": hl.hexdigest(),
-            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0"
-        }
+        liveurl = self.liveapi%(self.chname)
         try:
             response = requests.get(liveurl, headers=self.headers)
-            #response = requests.get(liveurl, headers=headers)
             response.raise_for_status()
         except requests.exceptions.RequestException as err:
             self.logger.error(err)
@@ -41,8 +26,16 @@ class gdtv_live(web_live):
         response.encoding = 'utf-8'
         try:
             info = json.loads(response.text)
-            link = info[0]['channel_stream'][0]['url']
-            link = link.replace('stream1','nclive')
+            if info["code"] != "200":
+                self.logger.error(info)
+                return None
+            link = info["body"]["urlInfo"]["url"]
+            link = link.replace('1200','2500')
+            link = link.replace('1500','3000')
+            link = link.replace('51','57')
+            u = urlparse(link)
+            result = u._replace(netloc='live.hcs.cmvideo.cn')
+            link = urlunparse(result)
             print("  {0: <20}{1:}".format(self.extinfo[4], link))
             channel = self.extinfo + [link] + [self.headers["Referer"] if self.referer == 1 else ""]
             self.link = link
